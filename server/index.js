55299039fa5f59590bd398ea4b2f07e93893223f
Nikita Volkov <nikitavolkov1707@gmail.com>
2021-02-24 00:45:17 +0300

    первая попытка запустить сервер с js отрисовкой

--- server/index.js
+++ server/index.js
@@ -7,14 +7,19 @@ app.use(express.static('src'));
 
 app.use('/favicon.ico', express.static('img/favicon.ico'));
 
-app.get('/login', (req, res) => {
-    console.log(req.ip, 'page request', req.url);
-    res.sendFile(path.resolve(`${__dirname}/../src/login.html`));
-});
+// app.get('/login', (req, res) => {
+//     console.log(req.ip, 'page request', req.url);
+//     res.sendFile(path.resolve(`${__dirname}/../src/login.html`));
+// });
+//
+// app.get('/signup', (req, res) => {
+//     console.log(req.ip, 'page request', req.url);
+//     res.sendFile(path.resolve(`${__dirname}/../src/signup.html`));
+// });
 
-app.get('/signup', (req, res) => {
+app.get('/*', (req, res) => {
     console.log(req.ip, 'page request', req.url);
-    res.sendFile(path.resolve(`${__dirname}/../src/signup.html`));
+    res.sendFile(path.resolve(`${__dirname}/../src/index.html`));
 });
 
 app.listen(port, () => {
