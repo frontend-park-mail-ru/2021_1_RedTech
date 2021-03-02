a9da9b3ad39399f64246143f999d3829a17f1cd1
Nikita Volkov <nikitavolkov1707@gmail.com>
2021-03-02 21:46:58 +0300

    RT-13: Добавить обработчик template.js

--- server/index.js
+++ server/index.js
@@ -7,6 +7,11 @@ app.use(express.static('src'));
 
 app.use('/favicon.ico', express.static('img/favicon.ico'));
 
+app.get('/templates.js', (req, res) => {
+    console.log(req.ip, 'page request', req.url);
+    res.sendFile(path.resolve(`${__dirname}/../templates.js`));
+});
+
 app.get('/*', (req, res) => {
     console.log(req.ip, 'page request', req.url);
     res.sendFile(path.resolve(`${__dirname}/../src/index.html`));
