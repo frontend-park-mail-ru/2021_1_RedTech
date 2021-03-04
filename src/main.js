b77b5c5c634db890e6f2fe6a8083f50d6571fd18
Nikita Volkov <nikitavolkov1707@gmail.com>
2021-03-04 14:31:50 +0300

    RT-29: Добавить функции рендера страниц авторизация/регистрация в main.js

--- src/main.js
+++ src/main.js
@@ -1,248 +1,116 @@
-import {FormComponent} from "./components/Form/Form.js";
+import {SignUpView} from "./views/SignUp/SignUp.js";
+import {LogInView} from "./views/LogIn/LogIn.js";
 
-const application = document.getElementById('app');
+const APPLICATION = document.getElementById('app');
 
-const config = {
-    menu: {
-        href: '/menu',
-        text: 'Меню!',
-        open: menuPage,
-    },
+const CONFIG = {
     signup: {
         href: '/signup',
         text: 'Зарегистрироваться',
-        open: signUpPage,
+        open
     },
     login: {
         href: '/login',
         text: 'Войти',
-        open: loginPage,
-    },
-    pug: {
-        href: '/pug',
-        text: 'pug',
-        open: pugPage,
-    }
-
-}
-
-const inputsForSignUp = {
-    login: {
-        placeholder: 'Логин',
-    },
-    email: {
-        placeholder: 'Email',
-        type: 'email',
-    },
-    password: {
-        placeholder: 'Пароль',
-        type: 'password',
-    },
-    confirmPassword: {
-        placeholder: 'Подтвердите пароль',
-        type: 'password',
+        open
     }
 }
 
-const inputsForLogin = {
-    email: {
-        placeholder: 'Email',
-        type: 'email',
-    },
-    password: {
-        placeholder: 'Пароль',
-        type: 'password',
-    }
+const configForSignUp = {
+    inputs: [
+        {
+            id: 'login',
+            placeholder: 'Логин',
+            type: 'text',
+        },
+        {
+            id: 'email',
+            placeholder: 'Email',
+            type: 'email',
+        },
+        {
+            id: 'password',
+            placeholder: 'Пароль',
+            type: 'password',
+        },
+        {
+            id: 'confirmPassword',
+            placeholder: 'Подтвердите пароль',
+            type: 'password',
+        }
+    ],
+    buttonName: 'Зарегистрироваться'
 }
 
-const networksAuth = {
-    google: {
-        src: 'img/google.svg',
-    },
-    facebook: {
-        src: 'img/facebook.svg',
-    },
-    vk: {
-        src: 'img/vk.svg',
-    }
+const configForLogin = {
+    inputs: [
+        {
+            placeholder: 'Email',
+            type: 'email',
+        },
+        {
+            placeholder: 'Пароль',
+            type: 'password',
+        }
+    ],
+    networksAuth: [
+        {
+            src: 'img/google.svg',
+        },
+        {
+            src: 'img/facebook.svg',
+        },
+        {
+            src: 'img/vk.svg',
+        }
+    ],
+    buttonName: 'Войти'
 }
 
-const appendTag = (tagName, className, text, parentName) => {
-    let tag = document.createElement(tagName);
+CONFIG.signup.open = () => {
+    APPLICATION.innerHTML = '';
 
-    if (className !== undefined) {
-        tag.className = className;
-    }
-
-    if (text !== undefined) {
-        tag.textContent = text;
-    }
-
-    if (parentName !== undefined) {
-        const elements = application.getElementsByClassName(parentName);
-        elements.item(0).appendChild(tag);
-    } else {
-        application.appendChild(tag);
-    }
-
-    return tag;
+    const signUpView = new SignUpView({
+        parent: APPLICATION,
+        data: configForSignUp
+    });
+    signUpView.render();
 }
 
-const fillFormTag = (tagName, className, text) => {
-    let tag = document.createElement(tagName);
+CONFIG.login.open = () => {
+    APPLICATION.innerHTML = '';
 
-    if (className !== undefined) {
-        tag.className = className;
-    }
-
-    if (text !== undefined) {
-        tag.textContent = text;
-    }
-
-    application.getElementsByTagName('form').item(0).appendChild(tag);
+    const logInView = new LogInView({
+        parent: APPLICATION,
+        data: configForLogin
+    });
+    logInView.render();
 }
 
-const fillInputs = (isSignUp) => {
-
-    let data = isSignUp ?  inputsForSignUp : inputsForLogin
+const menuPage = () => {
+    APPLICATION.innerHTML = '';
 
     Object
-        .entries(data)
-        .map(([inputKey, {placeholder, type}]) => {
-            const divForInput = appendTag('div','inputs__input-field',null, 'form-content__inputs');
-
-            const inputItem = document.createElement('input')
-            inputItem.className = 'input-field__input';
-            inputItem.placeholder = placeholder
-
-            if (type !== undefined) {
-                inputItem.type = type
-            }
-
-            divForInput.appendChild(inputItem)
-        })
-    ;
-}
-
-const createATag = (className, href, text) => {
-    const aTag = document.createElement('a');
-
-    if (className !== undefined) {
-        aTag.className = className;
-    }
-
-    if (href !== undefined) {
-        aTag.href = href;
-    }
-
-    if (text !== undefined) {
-        aTag.text = text
-    }
-
-    return aTag;
-}
-
-const fillAuth = () => {
-    appendTag('div', 'form-content__networks', null, 'form__form-content');
-    appendTag('div','networks__title', 'Или войдите с помощью социальных сетей', 'form-content__networks');
-    appendTag('div', 'networks__content', null, 'form-content__networks');
-
-    Object
-        .entries(networksAuth)
-        .map(([, {src}]) => {
-            const networkDiv = appendTag('div','networks__img',null, 'networks__content');
-            //const networkBtn = appendTag('button','networks__button',null, 'networks__img');
-
-            const networkBtn = document.createElement('button');
-            networkBtn.className = 'networks__button';
-            networkDiv.appendChild(networkBtn)
-
-            const inputItem = document.createElement('img')
-            inputItem.className = 'networks__img';
-            inputItem.src = src
-
-            networkBtn.appendChild(inputItem)
-        });
-
-}
-
-function signUpPage() {
-    application.innerHTML = '';
-    appendTag('div','login-signup');
-    appendTag('div','login-signup__content',null, 'login-signup');
-    appendTag('div','content__title', 'Регистрация', 'login-signup__content');
-    appendTag('div','content__title-hint', 'Зарегистрируйтесь для просмотра фильмов и сериалов', 'login-signup__content');
-    appendTag('div','content__form', null,'login-signup__content');
-    appendTag('div','form__form-content', null, 'content__form');
-    appendTag('form', null, null, 'form__form-content');
-
-    fillFormTag('div', 'form-content__inputs');
-    fillFormTag('div', 'form-content__hint', 'Не менее 6 символов');
-    fillFormTag('button', 'form-content__button', 'Зарегистрироваться');
-
-    fillInputs(true);
-
-    const divForHint = appendTag('div', 'form-content__have-acc', 'Уже зарегистрированы?', 'form__form-content');
-    divForHint.appendChild(createATag('have-acc__link', 'login.html', 'Войдите в аккаунт!'))
-
-}
-
-function loginPage() {
-    application.innerHTML = '';
-    appendTag('div','login-signup');
-    appendTag('div','login-signup__content',null, 'login-signup');
-    appendTag('div','content__title', 'Вход', 'login-signup__content');
-    appendTag('div','content__title-hint', 'Войдите для доступа к подписке и списку избранного', 'login-signup__content');
-    appendTag('div','content__form', null,'login-signup__content');
-    appendTag('div','form__form-content', null, 'content__form');
-    appendTag('form', null, null, 'form__form-content');
-
-    fillFormTag('div', 'form-content__inputs');
-    fillFormTag('div', 'form-content__hint', 'Забыли пароль?');
-    fillFormTag('button', 'form-content__button', 'Войти');
-
-    fillInputs(false);
-
-    fillAuth();
-
-    const divForHint = appendTag('div', 'form-content__have-acc', 'Еще нет аккаунта?', 'form__form-content');
-    divForHint.appendChild(createATag('have-acc__link', 'login.html', 'Зарегистрируйтесь!'));
-
-}
-
-function pugPage() {
-    application.innerHTML = '';
-
-    const formComponent = new FormComponent({
-        parent: application
-    })
-    formComponent.render()
-}
-
-function menuPage() {
-    application.innerHTML = '';
-
-    Object
-        .entries(config)
-        .map(([menuKey, {text, href}]) => {
+        .entries(CONFIG)
+        .map(([configKey, {text, href}]) => {
             const menuItem = document.createElement('a');
             menuItem.href = href;
             menuItem.textContent = text;
-            menuItem.dataset.section = menuKey;
+            menuItem.dataset.section = configKey;
 
             return menuItem;
         })
-        .forEach(element => application.appendChild(element))
+        .forEach(element => APPLICATION.appendChild(element))
     ;
 }
 
 menuPage();
 
-application.addEventListener('click', e => {
-    const {target} = e;
+APPLICATION.addEventListener(('click'), event => {
+    const {target} = event;
 
     if (target instanceof HTMLAnchorElement) {
-        e.preventDefault();
-        config[target.dataset.section].open();
+        event.preventDefault();
+        CONFIG[target.dataset.section].open();
     }
 });
