e00abaf72cf88f7af683c9c032d5660aa5519af6
Nikita Volkov <nikitavolkov1707@gmail.com>
2021-03-07 15:08:03 +0300

    RT-25: Запретить выполнение события по умолчани и добавить проверку на undefined

--- src/utils/validateForm.js
+++ src/utils/validateForm.js
@@ -1,12 +1,18 @@
 /**
  * Check validation of form.
  * Calls others functions to check validations of every type of input.
- * @param {HTMLFormElement} form - form for validation
+ * @param {Element} form - form for validation
  */
 export const validateForm = (form) => {
+    if(form === undefined) {
+        return;
+    }
+
     const inputs = form.querySelectorAll('.input-field__input');
 
-    form.addEventListener(('submit'), function (event) {
+    form.addEventListener(('submit'), event => {
+
+        event.preventDefault();
 
         inputs.forEach((input) => {
             document.getElementById(input.id + 'Error').textContent = '';
@@ -17,19 +23,20 @@ export const validateForm = (form) => {
 
             if(!input.value) {
                 errorDiv.textContent = 'Это поле обязательно к заполнению';
+                event.preventDefault();
             } else {
                 switch (input.id) {
                     case 'email':
-                        validateEmail(input.value, errorDiv, event);
+                        validateEmail(input.value, errorDiv);
                         break;
                     case 'login':
-                        validateLogin(input.value, errorDiv, event);
+                        validateLogin(input.value, errorDiv);
                         break;
                     case 'password':
-                        validatePassword(input.value, errorDiv, event);
+                        validatePassword(input.value, errorDiv);
                         break;
                     case 'confirmPassword':
-                        validateConfirmPassword(input.value, errorDiv, event);
+                        validateConfirmPassword(input.value, errorDiv);
                         break;
                 }
             }
@@ -41,9 +48,8 @@ export const validateForm = (form) => {
  * Check validation of inputted login.
  * @param {string} login - value of login input to be validated.
  * @param {HTMLElement} errorDiv - div element, where will be printed message of error.
- * @param {Event} event - in case of validation fail, event will be prevented.
  */
-const validateLogin = (login, errorDiv, event) => {
+const validateLogin = (login, errorDiv) => {
     if(!/^[a-zA-Z](.[a-zA-Z0-9]*)$/.test(login)) {
         errorDiv.textContent = 'Логин должен содержать только латинские буквы и цифры.' +
                                ' Первый символ логина может быть только буквенным';
@@ -55,9 +61,8 @@ const validateLogin = (login, errorDiv, event) => {
  * Check validation of inputted email.
  * @param {string} email - value of email input to be validated.
  * @param {HTMLElement} errorDiv - div element, where will be printed message of error.
- * @param {Event} event - in case of validation fail, event will be prevented.
  */
-const validateEmail = (email, errorDiv, event) => {
+const validateEmail = (email, errorDiv) => {
     if(!/^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/.test(email)) {
         errorDiv.textContent = 'Введен некорректный почтовый адрес';
         event.preventDefault();
@@ -68,9 +73,8 @@ const validateEmail = (email, errorDiv, event) => {
  * Check validation of inputted password.
  * @param {string} password - value of login input to be validated.
  * @param {HTMLElement} errorDiv - div element, where will be printed message of error.
- * @param {Event} event - in case of validation fail, event will be prevented.
  */
-const validatePassword = (password, errorDiv, event) => {
+const validatePassword = (password, errorDiv) => {
     if (password.length < 8 || password.length > 50) {
         errorDiv.textContent = 'Пароль должен быть длиной от 8 до 50 символов';
         event.preventDefault();
@@ -81,13 +85,11 @@ const validatePassword = (password, errorDiv, event) => {
  * Check validation of inputted password for confirmation.
  * @param {string} confirmPassword - value of password for confirmation input to be validated.
  * @param {HTMLElement} errorDiv - div element, where will be printed message of error.
- * @param {Event} event - in case of validation fail, event will be prevented.
  */
-const validateConfirmPassword = (confirmPassword, errorDiv, event) => {
+const validateConfirmPassword = (confirmPassword, errorDiv) => {
     const password = document.getElementById('password').value;
 
     if (confirmPassword !== password) {
         errorDiv.textContent = 'Пароли не совпадают!';
-        event.preventDefault();
     }
 }
