1108814fb78845952aaab24207316ac6dcb5503f
Nikita Volkov <nikitavolkov1707@gmail.com>
2021-03-08 15:59:42 +0300

    RT-29: Вынести конфиги из js в pug

--- src/views/LogIn/LogIn.js
+++ src/views/LogIn/LogIn.js
@@ -1,42 +1,13 @@
 import { APPLICATION } from '../../main.js';
 import { SignUpView } from '../SignUp/SignUp.js';
 
-const configForLogin = {
-    inputs: [
-        {
-            placeholder: 'Email',
-            type: 'email',
-        },
-        {
-            placeholder: 'Пароль',
-            type: 'password',
-        }
-    ],
-    networksAuth: [
-        {
-            src: 'img/google.svg',
-        },
-        {
-            src: 'img/facebook.svg',
-        },
-        {
-            src: 'img/vk.svg',
-        }
-    ],
-    buttonName: 'Войти'
-}
-
 /** Class representing a login page view. */
 export class LogInView {
     /**
      * Create a login page view.
-     * @param {HTMLElement} parent - Parent of signup view in DOM.
-     * @param {Object} data - Parameters for render signup view.
+     * @param {Object} data - Parameters for render login view.
      */
-    constructor({
-                    data = {},
-                } = {})
-    {
+    constructor({ data = {} } = {}) {
         this._data = data;
     }
 
@@ -44,12 +15,12 @@ export class LogInView {
      * Render html login page from pug template to parent.
      */
     render() {
-        const template = puglatizer.LogIn.LogIn(configForLogin);
+        const template = puglatizer.LogIn.LogIn();
         APPLICATION.innerHTML = template;
 
-        const aTag = document.getElementsByClassName('have-acc__link');
+        const [aTag] = document.getElementsByClassName('have-acc__link');
 
-        aTag.item(0).addEventListener(('click'), event => {
+        aTag.addEventListener(('click'), event => {
             event.preventDefault();
 
             APPLICATION.innerHTML = '';
--- src/views/SignUp/SignUp.js
+++ src/views/SignUp/SignUp.js
@@ -1,43 +1,13 @@
 import { APPLICATION } from '../../main.js';
 import { LogInView } from '../LogIn/LogIn.js';
 
-const configForSignUp = {
-    inputs: [
-        {
-            id: 'login',
-            placeholder: 'Логин',
-            type: 'text',
-        },
-        {
-            id: 'email',
-            placeholder: 'Email',
-            type: 'email',
-        },
-        {
-            id: 'password',
-            placeholder: 'Пароль',
-            type: 'password',
-        },
-        {
-            id: 'confirmPassword',
-            placeholder: 'Подтвердите пароль',
-            type: 'password',
-        }
-    ],
-    buttonName: 'Зарегистрироваться'
-}
-
 /** Class representing a signup page view. */
 export class SignUpView {
     /**
      * Create a signup page view.
-     * @param {HTMLElement} parent - Parent of signup view in DOM.
      * @param {Object} data - Parameters for render signup view.
      */
-    constructor({
-                    data = {},
-                } = {})
-    {
+    constructor({ data = {} } = {}) {
         this._data = data;
     }
 
@@ -45,12 +15,12 @@ export class SignUpView {
      * Render html signup page from pug template to parent.
      */
     render() {
-        const template = puglatizer.SignUp.SignUp(configForSignUp);
+        const template = puglatizer.SignUp.SignUp();
         APPLICATION.innerHTML = template;
 
-        const aTag = document.getElementsByClassName('have-acc__link');
+        const [aTag] = document.getElementsByClassName('have-acc__link');
 
-        aTag.item(0).addEventListener(('click'), event => {
+        aTag.addEventListener(('click'), event => {
             event.preventDefault();
 
             APPLICATION.innerHTML = '';
