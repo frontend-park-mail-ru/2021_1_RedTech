625bf003a22b47a37eaf300fcab8f905daef54bf
Nikita Volkov <nikitavolkov1707@gmail.com>
2021-03-10 01:33:10 +0300

    RT-34: Добавить стиль для отключенного лэйбла

--- src/views/Profile/Profile.js
+++ src/views/Profile/Profile.js
@@ -4,16 +4,16 @@ import { isValidForm } from '../../utils/isValidForm.js';
 /** Class representing a login page view. */
 export class ProfileView {
     /**
-     * Create a profile page view.
-     * @param {Object} data - Parameters for render profile view.
-     */
-    constructor({ data = {} } = {}) {
+	 * Create a profile page view.
+	 * @param {Object} data - Parameters for render profile view.
+	 */
+    constructor({data = {}} = {}) {
         this._data = data;
     }
 
     /**
-     * Render html login page from pug template to parent.
-     */
+	 * Render html login page from pug template to parent.
+	 */
     render() {
         const template = puglatizer.Profile.Profile(this._data);
         APPLICATION.innerHTML = template;
@@ -29,24 +29,30 @@ export class ProfileView {
             if (button.textContent === 'Редактировать') {
                 button.textContent = 'Сохранить';
                 inputs.forEach((input) => {
-                    if (input.id === 'file') {
-                        input.disabled = false;
-                    } else {
-                        input.readOnly = false;
+                    if (input.tagName === 'LABEL') {
+                        input.classList.remove('input-wrapper__input_disabled');
                     }
 
+                    if (input.tagName !== 'BUTTON') {
+                        input.disabled = false;
+                    }
                 });
             } else if (button.textContent === 'Сохранить') {
                 const isValid = isValidForm(form);
                 if (isValid) {
+                    const [nick] = document.getElementsByClassName('title-wrapper__nickname');
+
+                    nick.textContent = document.getElementById('login').value;
                     button.textContent = 'Редактировать';
+
                     inputs.forEach((input) => {
-                        if (input.id === 'file') {
-                            input.disabled = true;
-                        } else {
-                            input.readOnly = true;
+                        if (input.tagName === 'LABEL') {
+                            input.classList.add('input-wrapper__input_disabled');
                         }
 
+                        if (input.tagName !== 'BUTTON') {
+                            input.disabled = true;
+                        }
                     });
                 }
             }
