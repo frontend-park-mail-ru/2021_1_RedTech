a9fab38992e424d0f49dd210c343cfb8e37d8284
Nikita Volkov <nikitavolkov1707@gmail.com>
2021-04-02 23:37:50 +0300

    RT-78: Изменить main для реализации MVC

--- src/main.js
+++ src/main.js
@@ -1,98 +1,17 @@
-import { DetailComponent } from './views/DetailView/DetailView.js';
-import { HomeComponent } from './views/HomeView/HomeView.js';
-import { ProfileView } from './views/Profile/Profile.js';
-import { SignUpView } from './views/SignUp/SignUp.js';
-import { LogInView } from './views/LogIn/LogIn.js';
+import eventBus from './modules/eventBus.js';
+import { LogInController } from './controllers/logIn.js';
+import { SignUpController } from './controllers/signUp.js';
+import { ProfileController } from './controllers/profile.js';
+import { HomePageController } from './controllers/homePage.js';
+import { DetailPageController } from './controllers/detailPage.js';
 
 export const APPLICATION = document.getElementById('app');
 
-/**
- * Render signup page and check validation of form.
- */
-const signUpPage = () => {
-    APPLICATION.innerHTML = '';
+/*eslint no-unused-vars: */
+const signupController = new SignUpController();
+const loginController = new LogInController();
+const profileController = new ProfileController();
+const homePageController = new HomePageController();
+const detailPageController = new DetailPageController();
 
-    const signUpView = new SignUpView();
-    signUpView.render();
-};
-
-/**
- * Render login page and check validation of form.
- */
-const loginPage = () => {
-    APPLICATION.innerHTML = '';
-
-    const logInView = new LogInView();
-    logInView.render();
-};
-
-/**
- * Render login page and check validation of form.
- */
-const profilePage = () => {
-    APPLICATION.innerHTML = '';
-
-    const profileView = new ProfileView();
-    profileView.render();
-};
-
-/**
- * Render home page with all recommended content.
- */
-const homePage = () => {
-    APPLICATION.innerHTML = '';
-
-    const homeComponent = new HomeComponent();
-    homeComponent.render();
-};
-
-/**
- * Render detail page with detail information about media.
- */
-const detailPage = () => {
-    APPLICATION.innerHTML = '';
-
-    const formComponent = new DetailComponent();
-    formComponent.render();
-};
-
-const MENU = {
-    signup: {
-        href: '/signup',
-        text: 'Зарегистрироваться',
-        open: signUpPage,
-    },
-    login: {
-        href: '/login',
-        text: 'Войти',
-        open: loginPage,
-    },
-    profile: {
-        href: '/login',
-        text: 'Профиль',
-        open: profilePage,
-    },
-    home: {
-        href: '/home',
-        text: 'Домашняя страница',
-        open: homePage,
-    },
-    detail: {
-        href: '/detail',
-        text: 'Детальная страница о кино',
-        open: detailPage,
-    }
-};
-
-APPLICATION.addEventListener(('click'), event => {
-    const { target } = event;
-
-    const [mainPageHref] = APPLICATION.getElementsByClassName('main-page__href');
-
-    if (target instanceof HTMLAnchorElement && APPLICATION.contains(mainPageHref)) {
-        event.preventDefault();
-        MENU[target.dataset.section].open();
-    }
-});
-
-homePage();
+eventBus.emit('homepage:render');
