9681e3b5feb2720a096490da5a438ed3b0e8aeca
Nikita Volkov <nikitavolkov1707@gmail.com>
2021-04-02 23:36:22 +0300

    RT-78: Исправить опечатку в eslintrs

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
         'es2021': true
     },
     'globals': {
-      'puglatizer': true
+        'puglatizer': true
     },
     'extends': 'eslint:recommended',
     'parserOptions': {
