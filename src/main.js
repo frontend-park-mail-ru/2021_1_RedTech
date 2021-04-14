580b21322a158445ba28d11c26626cc79b06d604
Timonin Anton <dotruger.37@gmail.com>
2021-04-14 17:00:42 +0300

    RT-79: Исправить сборку bundle

--- src/main.js
+++ src/main.js
@@ -26,7 +26,7 @@ const genrePageController = new GenrePageController();
 const favouritesPageController = new FavouritesPageController();
 
 if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/dist/sw.js');
+    navigator.serviceWorker.register('sw.js');
 }
 
 const router = new Router(APPLICATION);
