711208578c80aae75c0c1641dd394fb83bf2fc86
Nikita Volkov <nikitavolkov1707@gmail.com>
2021-05-26 18:37:29 +0300

    RT-126: Сделать так, чтоб линтер проверял весь проект целиком

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
+// eslint-disable-next-line no-undef
 module.exports = {
     'env': {
         'browser': true,
         'es2021': true
     },
     'globals': {
-        'puglatizer': true
+        'puglatizer': true,
+        'require': true
     },
     'extends': 'eslint:recommended',
     'parserOptions': {
